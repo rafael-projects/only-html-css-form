960edf186762294a13c853b575100a2162a39488
rafael-projects <rss4110@hotmail.com>
2023-01-28 02:50:45 -0300

    update html and css, now, rendering form sign in

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+
+function myToggle(){
+  let menuToggle = document.querySelector(".menuToggle");
+  let navigation = document.querySelector(".navigation");
+
+  menuToggle.onclick= function() 
+  {
+    navigation.classList.toggle('active')  
+  }
+}
